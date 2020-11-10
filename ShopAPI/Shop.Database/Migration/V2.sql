a2ef27bfab25555c32dec86b3cd3268fe4b733ef
BrusentsoV <V>
2020-11-10 23:03:34 +0300

    DbVersion, Post-Deploy script, Attribute and Values tables added

---
+++ ShopAPI/Shop.Database/Migration/V2.sql
@@ -0,0 +1,39 @@
+﻿if (select top(1) Version from dbo.DbVersion) is null 
+insert into dbo.DbVersion values(sysutcdatetime(),1)
+
+declare @dbVersion int 
+
+Select top(1) @dbVersion = Version 
+from dbo.DbVersion 
+order by Created DESC
+if @dbVersion >=2 set noexec on
+
+CREATE TABLE [dbo].[Attribute](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](20) NOT NULL,
+ CONSTRAINT [PK_Attribute] PRIMARY KEY CLUSTERED ([Id] ASC),
+ CONSTRAINT [AK_Attribute_Name] UNIQUE NONCLUSTERED ([Name] ASC)
+)
+GO
+
+CREATE TABLE [dbo].[Value](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ProductId] [int]  NOT NULL,
+	[AttributeId] [int]  NOT NULL,
+	[Value] [nvarchar](15) NOT NULL,
+ CONSTRAINT [PK_Value] PRIMARY KEY CLUSTERED ([Id] ASC),
+ CONSTRAINT [AK_Value_ProductId_AttributeId] UNIQUE NONCLUSTERED ([ProductId] ASC, [AttributeId] ASC)
+)
+GO
+
+
+
+
+
+
+
+
+
+insert into dbo.DbVersion values(sysutcdatetime(),2)
+
+set noexec off
---
+++ ShopAPI/Shop.Database/dbo/Tables/DbVersion.sql
@@ -0,0 +1,6 @@
+﻿create table [dbo].[DbVersion] 
+(
+   [Id] int NOT NULL PRIMARY KEY Identity (1,1),
+    [Created]   datetime2 (0)  constraint DF_DbVersion_Created default sysutcdatetime() NOT NULL, 
+    [Version] decimal(18,2)  not null 
+);
